72c41d0a5622b7fad2b6b9008972c4a38bd53dc1
Ashley <a.clarkem@gmail.com>
2014-10-27 09:31:42 -0600

    did some stuff

---
+++ lib/numbers.rb
@@ -0,0 +1,8 @@
+class Numbers
+  def spiralize(start_number, width)
+    # first row is start, start + 1 times width
+    # second row is width times 4 minus..something, + 1 times width
+
+  end
+
+end
